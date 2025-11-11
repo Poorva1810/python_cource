3e984482ba8af0f7ab6e66f2fdbc5ec1b47c1c92
Poorva1810 <harshida849@gmail.com>
2025-11-11 21:21:42 +0530

    home work

--- homework/activity3.py
+++ homework/activity3.py
@@ -7,7 +7,7 @@ def term(number):
         return term(number-1)+term(number-2)
 number=int(input("enter the number of term of fibonacci series"))
 if number<=0:
-    print("series dose not axist for negative number")
+    print("series dose not axist for negative number of term")
 else:
     print("the term of the series is",term(number))
 
