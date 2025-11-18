3d03013a2353764ff9b9622d444e887c61077f4e
Poorva1810 <harshida849@gmail.com>
2025-11-18 22:17:49 +0530

    lesson6

---
+++ lesson6/activity3.py
@@ -0,0 +1,6 @@
+set1={"green","blue"}
+set2={"blue","yellow"}
+print(set1)
+print(set2)
+union_set=set1.union(set2)
+print("union: ",union_set)
---
+++ lesson6/activity4.py
@@ -0,0 +1,6 @@
+set1={"blue","green"}
+set2={"blue","yellow"}
+print(set1)
+print(set2)
+set3=set1.intersection(set2)
+print("intersection: ",set3)
