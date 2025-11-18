f27c980b991402ee24936eb097ae2bf9428db2b3
Poorva1810 <harshida849@gmail.com>
2025-11-18 20:20:31 +0530

    homework done

---
+++ homework/activity6.py
@@ -0,0 +1,11 @@
+lst=[1,2,3,4,5,6,7,8]
+lst.remove(4)
+number=int(input("enter the number"))
+if number==4:
+    print("you are correct")
+elif number>4:
+    print("your number is higher than that number")
+else:
+    print("your number is smaller than that number")
+
+
