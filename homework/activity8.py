8995588395d17108571fd348818736a746d44d94
Poorva1810 <harshida849@gmail.com>
2025-11-25 19:55:35 +0530

    homework done

---
+++ homework/activity8.py
@@ -0,0 +1,9 @@
+class robot:
+    name="Ramsingh-070"
+    def introduction(self):
+        print("hey i am a robot")
+    def details(self):
+        print("my name is: ",self.name)
+ob=robot()
+ob.introduction()
+ob.details()  
