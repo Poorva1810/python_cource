3dcd12ffd48eef6b065662c4c2fce0b1a4113e3a
Poorva1810 <harshida849@gmail.com>
2026-01-08 19:51:31 +0530

    homework done

---
+++ homework/activity19.py
@@ -0,0 +1,3 @@
+binary_num = input("Enter a binary number: ")
+decimal = int(binary_num, 2)
+print("Decimal value:", decimal)
---
+++ lesson18/activity3.py
@@ -0,0 +1,22 @@
+def roman_to_int(s):
+    roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    total = 0
+    prev_value = 0
+    for i in reversed(s):
+        value = roman[i]
+        if value < prev_value:
+            total -= value
+        else:
+            total += value
+        prev_value = value
+    return total
+roman_numeral = input("Enter a Roman numeral: ").upper()
+print("Integer value:", roman_to_int(roman_numeral))
