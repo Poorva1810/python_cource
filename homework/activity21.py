619a785871a64338a0fe1bcea008ebb1a404e05d
Poorva1810 <harshida849@gmail.com>
2026-01-20 19:48:32 +0530

    homework done

---
+++ homework/activity21.py
@@ -0,0 +1,12 @@
+print("Two-digit prime numbers are:")
+for num in range(10, 100):
+    is_prime = True
+    if num < 2:
+        is_prime = False
+    else:
+        for i in range(2, num):
+            if num % i == 0:
+                is_prime = False
+                break
+    if is_prime:
+        print(num, end=" ")
